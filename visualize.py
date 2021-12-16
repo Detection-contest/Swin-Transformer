2de7a4bced3f31c5a5c566b056e6420364bd1d5c
NaYoung2253 <55417591+NaYoung2253@users.noreply.github.com>
2021-12-17 01:27:28 +0900

    Add visualize.py

---
+++ visualize.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot, async_inference_detector
+import asyncio
+ 
+df = pd.read_csv('dataset/ImageSets/Main/val.txt', names=['jpg'])
+
+visual_root = "dataset/swin_VisualImage/"
+image_root = "dataset/JPEGImages/"
+point_root = "Swin-Transformer-Object-Detection/work_dirs/cascade_mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco/"
+
+# only epoch 14
+config_file = (point_root + 'cascade_mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py')
+checkpoint_file = (point_root + 'epoch_14.pth')
+
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+async def async_main():
+    for j in range(0, 100):
+        img = (image_root + df.loc[j]['jpg'] + '.jpg')
+        result = inference_detector(model, img)
+        model.show_result(img, result, out_file=(visual_root + 'epoch_14_' + df.loc[j]['jpg'] + '.jpg'))
+
+
+if __name__ == '__main__':
+    await async_main()
