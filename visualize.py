0fe7a85cb948ead2b0e649e464b2831be38bcf3b
NaYoung2253 <55417591+NaYoung2253@users.noreply.github.com>
2021-12-17 01:57:52 +0900

    modified visualize.py

--- visualize.py
+++ visualize.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot, async_inference_detector
 import asyncio
+import os
  
 df = pd.read_csv('dataset/ImageSets/Main/val.txt', names=['jpg'])
 
 visual_root = "dataset/swin_VisualImage/"
+if (os.path.isdir(visual_root) == False):
+    make_root_dir = visual_root
+    os.makedirs(make_root_dir)
+
 image_root = "dataset/JPEGImages/"
 point_root = "Swin-Transformer-Object-Detection/work_dirs/cascade_mask_rcnn_swin_tiny_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco/"
 
@@ -14,7 +19,7 @@ checkpoint_file = (point_root + 'epoch_14.pth')
 
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-async def async_main():
+def async_main():
     for j in range(0, 100):
         img = (image_root + df.loc[j]['jpg'] + '.jpg')
         result = inference_detector(model, img)
@@ -22,4 +27,4 @@ async def async_main():
 
 
 if __name__ == '__main__':
-    await async_main()
+    async_main()
